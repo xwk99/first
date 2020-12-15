9594686f27d7f9a8f1b321199587096a77f0733a
“xwk” <1246960399@qq.com>
2020-12-15 09:13:09 +0800

    city-list

--- src/views/home/components/header.vue
+++ src/views/home/components/header.vue
@@ -5,7 +5,8 @@
       </div>
       <div class="header-center">
            <span class="iconfont find-icon">&#xe632;</span>
-           输入城市/景点/游玩主题</div>
+           <input type="text" class="find-input"  placeholder="输入城市/景点/游玩主题" />
+           </div>
       <router-link to="/city">
             <div class="header-right">
                 城市
@@ -34,9 +35,6 @@ export default {
             width: .54rem
             padding-left: 0.1rem
             float: left
-            .find-icon
-                text-align: center
-                font-size: .4rem
         .header-center
             flex: 1
             margin-top: .12rem
@@ -46,6 +44,17 @@ export default {
             background: white
             border-radius: .1rem
             color: gray
+            position: relative
+            overflow: hidden
+            .find-icon
+                text-align: center
+                font-size: .4rem
+            .find-input
+                height: .64rem
+                width: 86%
+                position: absolute
+                left: 10%
+                padding: 0 .1rem 0 .1rem
         .header-right
             width: 1.24rem
             float: right
